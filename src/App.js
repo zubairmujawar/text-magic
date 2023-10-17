2bfa94569918152714cdf29ac9632eccb74b66fb
Zubair Mujawar <mujawarzubair167@gmail.com.com>
2023-10-17 22:25:42 +0530

    Changes in css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Textmagic from "./Components/Textmagic";
+import Navbar from "./Components/Navbar";
 
 function App() {
 
   return (
    <>
+    <Navbar/>
     <Textmagic/>
    </>
   );

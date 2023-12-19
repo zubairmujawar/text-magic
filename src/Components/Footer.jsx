ed49171638d828aae709268a8b9bce4637324382
Zubair Mujawar <mujawarzubair167@gmail.com.com>
2023-12-19 22:28:28 +0530

    changes for live deployment

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,9 +3,9 @@ const Footer = () => {
   return (
     <div className='footer'>
       <p>Follow us on</p>
-      <img src="../instagram.png"/>
-      <img src="../facebook.png"/>
-      <img src="../twitter.png"/>
+      <img src="../instagram.png" alt="logo"/>
+      <img src="../facebook.png" alt="logo"/>
+      <img src="../twitter.png" alt="logo"/>
     </div>
   )
 }
